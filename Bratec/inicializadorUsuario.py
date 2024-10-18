0c953bfae076908191a679a093cb9562f222555f
JesseGenteBoa <153686378+JesseGenteBoa@users.noreply.github.com>
2024-10-18 18:06:07 -0300

    Update inicializadorUsuario.py

--- Bratec/inicializadorUsuario.py
+++ Bratec/inicializadorUsuario.py
@@ -1,14 +1,16 @@
-from pyautogui import hotkey, press, write, FAILSAFE, FailSafeException
+from pyautogui import hotkey, FAILSAFE, FailSafeException
 from pyperclip import paste
 from time import sleep
 from selenium import webdriver                         
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 import utils
 import pyscreeze
          
 
-FAILSAFE = False
+FAILSAFE = True
+
 
 def inicializarUsuario():
     ver_documento = r'Imagens\verDocumentos.png'
@@ -27,23 +29,28 @@ def inicializarUsuario():
     sleep(0.5)      
     link = paste()
     options = webdriver.ChromeOptions()
-    options.add_argument(r'user-data-dir=C:\Users\User\AppData\Local\Google\Chrome\User Data\Perfil Selenium')
+    options.add_argument(r'user-data-dir=C:\Users\Usuario\AppData\Local\Google\Chrome\User Data\Profile Selenium')
     servico = Service(ChromeDriverManager().install())
     driver = webdriver.Chrome(service=servico, options=options)
     driver.get(link)
-    utils.checarFailsafe()
         
         
     sleep(2)
-    press(["tab"]*3)
-    write("bot.contabil")
-    press("tab")
-    write("EQSeng852@")
-    press("enter")
+    try:
+        usuario = driver.find_element(By.XPATH, '/html/body/app-root/app-login/po-page-login/po-page-background/div/div/div[2]/div/form/div/div[1]/div[1]/po-login/po-field-container/div/div[2]/input')
+        usuario.send_keys("bot.contabil")
+    except:
+        driver.quit()
+        sleep(1)
+        hotkey("ctrl", "w")
+        raise FailSafeException
+    senha = driver.find_element(By.XPATH, '/html/body/app-root/app-login/po-page-login/po-page-background/div/div/div[2]/div/form/div/div[2]/div[1]/po-password/po-field-container/div/div[2]/input')
+    senha.send_keys("EQSeng852@")
+    logar = driver.find_element(By.XPATH, '/html/body/app-root/app-login/po-page-login/po-page-background/div/div/div[2]/div/form/div/po-button/button')
+    logar.click()
     sleep(2)
     hotkey("alt", "tab", interval=0.1)
     hotkey("ctrl", "w")
     hotkey("alt", "tab", interval=0.1)
     sleep(7)
-    utils.checarFailsafe()
 
